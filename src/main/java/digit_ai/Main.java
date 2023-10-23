b78b8272a0af756b71fde22f29ad7bef922d8dfe
ivana <mmandreimm47@gmail.com>
2023-10-23 12:47:57 +0300

    Maler

--- src/main/java/digit_ai/Main.java
+++ src/main/java/digit_ai/Main.java
@@ -4,6 +4,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Pula");
+        for(int i = 0; i < 3; i++)
+        {
+            System.out.print(i+1 + " ");
+        }
     }
 }
